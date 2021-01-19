4460828c7431a1c1acb8e97d998a0fcdb1a306c9
be-15 <cezarpn@ufrj.br>
2021-01-19 20:26:09 -0300

    TAC1

--- TAC1/TAC1_testes.py
+++ TAC1/TAC1_testes.py
@@ -1,6 +1,25 @@
+'''''''''
 seq = input('Digite a sequência: ')
 codon_lista = []
 for i in range(0, len(seq), 3):
     codon_lista.append(seq[i:i+3])
 
 print(codon_lista)
+'''''''''
+seq=input('Digite a sequência:')
+contador=1
+lista=[]
+seq=seq.upper()
+
+for x in range(1, len(seq)):
+    if seq[x] == seq[x-1]:
+        contador += 1
+        if contador == 3:
+                posicao = x-1
+    elif seq[x] != seq[x-1]:
+        if contador >= 3:
+            lista.append((seq[x-1], contador, posicao))
+            count=1
+        else:
+            count=1
+print(lista)
